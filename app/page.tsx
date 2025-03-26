d7bfd96d5eb68c1a2555aadf0efbf6376cccddc0
varshith pilli <varshith6946@gmail.com>
2025-03-26 18:40:37 +0530

    changes-1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from "./Components/Navbar"
-import Header from "./Components/Header"
-import About from "./Components/About"
-import Contact from "./Components/Contact"
-import Footer from "./Components/Footer"
-import Skills from "./Components/Skills"
-import Projects from "./Components/Projects"
+import Navbar from "./Components/Navbar";
+import Header from "./Components/Header";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Skills from "./Components/Skills";
+import Projects from "./Components/Projects";
 export default function Home() {  
   return (
     <>
